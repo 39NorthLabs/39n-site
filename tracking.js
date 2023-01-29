e9dcaa80ff6c478e0b4f2c0297a568581b079f0b
Gavin Rozzi <gr@gavinrozzi.com>
2023-01-29 11:25:01 -0500

    Add tracking code and update Quarto version

---
+++ tracking.js
@@ -0,0 +1,16 @@
+<script>
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+  _paq.push(["setCookieDomain", "*.39n.io"]);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.gavinrozzi.com/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '20']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//analytics.gavinrozzi.com/matomo.php?idsite=20&amp;rec=1" style="border:0;" alt="" /></p></noscript>
